7be76ec77f3bbb34cd2ac33bdf01c3013b3fdc9c
SirMoM <pirat99@online.de>
2019-12-23 11:13:30 +0100

    init add lib

---
+++ PGUI/src/test/java/de/pgui/TestMain.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package de.pgui;
+
+import processing.core.PApplet;
+
+/**
+ * @author Noah Ruben
+ *
+ *
+ * @created 23.12.2019
+ */
+public class TestMain extends PApplet{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
